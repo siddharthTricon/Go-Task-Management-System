00c77475cb8811a921fa4c4fcffa4e00c89d9e9f
Siddharth Goswami <siddharth.goswami@Triconinfotech.com>
2025-01-16 16:29:58 +0530

    Updated the controller.go file

--- taskmanager/controllers/task_controller.go
+++ taskmanager/controllers/task_controller.go
@@ -5,6 +5,8 @@ import (
 	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gofiber/utils"
+	"github.com/siddharthTricon/go-task-management-sysytem/models"
 	"github.com/siddharthTricon/go-task-management-sysytem/repositories"
 )
 
@@ -15,3 +17,28 @@ type TaskController struct{
 func NewTaskController(repo repositories.TaskRepository) *TaskController{
 	return &TaskController{repo:repo}
 }  
+
+func (tc *TaskController) GetAllTasks(c *gin.Context){
+	tasks,  err := tc.repo.GetAllTasks()
+
+	if err != nil{
+		utils.RespondJSON(c, http.StatusInternalServerError, gin.H{"error":err.Error()})
+		return
+	}
+	utils.RespondJSON(c, http.StatusOK, tasks)
+}
+
+func (tc *TaskController) CreateTask(c *gin.Context){
+	var task models.Task
+
+	if err := c.BindJSON(&task); err != nil{
+		utils.RespondJSON(c, http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := tc.repo.CreateTask(task); err != nil{
+		utils.RespondJSON(c, http.StatusInternalServerError, gin.H{"error":err.Error()})
+		return
+	}
+	utils.RespondJSON(c, http.StatusCreated, task)
+}
