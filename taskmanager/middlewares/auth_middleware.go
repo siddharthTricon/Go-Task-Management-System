e8a2368980e11fd3eaffb344b8ec40aecbbb06c4
Siddharth Goswami <siddharth.goswami@Triconinfotech.com>
2025-01-17 16:08:18 +0530

    Added a middleware file

---
+++ taskmanager/middlewares/auth_middleware.go
@@ -0,0 +1,19 @@
+package middlewares
+
+import(
+    "net/http"
+    "github.com/gin-gonic/gin"
+    "github.com/siddharthTricon/go-task-management-sysytem/utils
+")
+
+func AuthMiddleware() gin.Handlerfunc{
+    return func(c *gin.Context){
+        token := c.GetHeader("Authorization")
+        if token == "" || !utils.ValidateToken(token){
+            c.JSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
+            c.Abort()
+            return
+        }
+        c.Next()
+    }
+}
