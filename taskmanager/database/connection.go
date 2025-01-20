9ca37dfcfa481ec3242700ae4a93cee7dc315b70
Siddharth Goswami <siddharth.goswami@Triconinfotech.com>
2025-01-20 18:55:28 +0530

    Updated the connection file

--- taskmanager/database/connection.go
+++ taskmanager/database/connection.go
@@ -9,13 +9,15 @@ import (
 
 var DB *gorm.DB
 
-func connect(){
-	dsn := "root:SIddharth@03@/taskmanager?charset=utf8&parseTime=True&loc=Local" 
+func Connect(){
+	dsn := "root:Siddharth@03@/taskmanager?charset=utf8&parseTime=True&loc=Local" 
 	var err error
-	DB, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil{
 		log.Fatal("Failed to connect to the database: ", err)
 	}
+
+	DB = db
 
 	log.Println("Database connected successfully")
 
