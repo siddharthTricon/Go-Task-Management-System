741bef5c51f1502a4caab5763215bf4885a1566b
Siddharth Goswami <siddharth.goswami@Triconinfotech.com>
2025-01-20 18:54:33 +0530

    Updated the Task controller file

--- taskmanager/controllers/task_controller.go
+++ taskmanager/controllers/task_controller.go
@@ -2,12 +2,10 @@ package controllers
 
 import (
 	"net/http"
-	"strconv"
-
 	"github.com/gin-gonic/gin"
-	"github.com/gofiber/utils"
 	"github.com/siddharthTricon/go-task-management-sysytem/models"
 	"github.com/siddharthTricon/go-task-management-sysytem/repositories"
+	"github.com/siddharthTricon/go-task-management-sysytem/utils"
 )
 
 type TaskController struct{
@@ -22,23 +20,23 @@ func (tc *TaskController) GetAllTasks(c *gin.Context){
 	tasks,  err := tc.repo.GetAllTasks()
 
 	if err != nil{
-		utils.RespondJSON(c, http.StatusInternalServerError, gin.H{"error":err.Error()})
+		utils.RespondJSON(c, http.StatusInternalServerError,"Error fetching task", gin.H{"error":err.Error()})
 		return
 	}
-	utils.RespondJSON(c, http.StatusOK, tasks)
+	utils.RespondJSON(c, http.StatusOK,"Task fetched successfully", tasks)
 }
 
 func (tc *TaskController) CreateTask(c *gin.Context){
 	var task models.Task
 
 	if err := c.BindJSON(&task); err != nil{
-		utils.RespondJSON(c, http.StatusInternalServerError, gin.H{"error": err.Error()})
+		utils.RespondJSON(c, http.StatusInternalServerError,"Invalid task data", gin.H{"error": err.Error()})
 		return
 	}
 
 	if err := tc.repo.CreateTask(task); err != nil{
-		utils.RespondJSON(c, http.StatusInternalServerError, gin.H{"error":err.Error()})
+		utils.RespondJSON(c, http.StatusInternalServerError,"Error creating task", gin.H{"error":err.Error()})
 		return
 	}
-	utils.RespondJSON(c, http.StatusCreated, task)
+	utils.RespondJSON(c, http.StatusCreated,"Task created", task)
 }
